2daf4d2186e3c16ca8ce8b241cbfb707e64960e5
Prerana Bhoi <preranabhoi32@gmail.com>
2025-10-12 14:39:08 +0000

    datatypes and variables done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=144553
+let accountEmail="prerana@google.com"
+var accountPassword="12345"
+accountCity="Dhule"
+let accountState;
+// accountId=2 //not allowed
+
+accountEmail="p@googlr.com"
+accountPassword="123123"
+accountCity="Pune"
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
