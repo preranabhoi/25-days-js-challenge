983b2b18602e505b775d2c10ecca27906ac65f4a
Prerana Bhoi <preranabhoi32@gmail.com>
2025-10-13 15:30:01 +0000

    datatypes and ECMA standards revised

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,10 @@
+"Use strict";//treat all js code as newer version
+
+let name="prerana"
+let age=18
+let isLoggedIn=false
+let state;
+
+console.log(typeof(age))
+console.log(typeof(null))
+console.log(typeof(undefined))
