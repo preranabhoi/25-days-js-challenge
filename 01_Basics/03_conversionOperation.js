e70558f784013f14f53fbf82a3f7394afca09019
Prerana Bhoi <preranabhoi32@gmail.com>
2025-10-14 16:22:59 +0000

    Datatype conversion done

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score="33abc"
+console.log(typeof(score))
+
+let valueInNumber=Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
