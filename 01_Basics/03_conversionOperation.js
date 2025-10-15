b418b2059ec660f58ff019cc07ab725c7607b1b0
Prerana Bhoi <preranabhoi32@gmail.com>
2025-10-15 11:21:07 +0000

    datatype conversion revised

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -14,3 +14,37 @@ let someNumber=33
 let stringNumber=String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+
+
+
+//operations
+
+let value=3
+let negValue=-value
+console.log(negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2/2)
+console.log(2%3)
+
+
+let str1="hello"
+let str2="prerana"
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+
+console.log((3+4)*5%3)
+
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter)
