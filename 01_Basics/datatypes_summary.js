e28dc60717fe616d4e21b57d6ea7882e730f9b44
Prerana Bhoi <preranabhoi32@gmail.com>
2025-10-17 14:26:39 +0000

    learned primitive and non-primitive datatypes

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,33 @@
+// Primitive
+//String,number,boolean,null,undefined,symbol,bigint
+
+const score=100
+const scoreValue=100.3
+
+const isLoggesIn=false
+const outsideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id===anotherId)
+
+const bigNumber=3434334535354534534564646464
+
+///Non-Primitive(Reference)
+//array,objects,functions
+
+const fruits=["apple","banana","grapes"]
+
+let myObj={name:"prerana",
+    age:18,
+    }
+
+
+    const myFunction=function(){
+    console.log("hello world");
+    }
+
+
+    console.log(typeof outsideTemp)
